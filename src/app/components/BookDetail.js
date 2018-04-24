f3de8413c9567ea187201adde27f776bcf4aacf8
emu <himtiaz@dohatec.com.bd>
2018-04-24 16:31:42 +0000

    Rating change using states

--- src/app/components/BookDetail.js
+++ src/app/components/BookDetail.js
@@ -1,6 +1,19 @@
 import React from 'react';
 
 export class BookDetail extends React.Component {
+  constructor(props){
+    super();
+    this.state = {
+      rate: props.detail.rating
+    }
+  }
+
+  upvote(){
+    this.setState({
+      rate: this.state.rate + 1
+    });
+  }
+
   render() {
     return (
       <div className="col-md-6">
@@ -10,10 +23,10 @@ export class BookDetail extends React.Component {
             <h3 className="mb-0">
               <a className="text-dark" href="#">{this.props.detail.name}</a>
             </h3>
-            <div className="mb-1 text-muted">Nov 11</div>
+            <div className="mb-1 text-muted">Rating: {this.state.rate}</div>
             <p className="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to
               additional content.</p>
-            <a href="#">Continue reading</a>
+            <a onClick={() => this.upvote()}>Upvote</a>
           </div>
           <img className="card-img-right flex-auto d-none d-lg-block book-cover"
                alt="Thumbnail [200x250]"
--- src/app/index.js
+++ src/app/index.js
@@ -5,51 +5,17 @@ import 'bootstrap/dist/js/bootstrap.js';
 import styles from './styles/blog.css';
 
 import { Header } from './components/Header';
-import { Scroller } from './components/Scroller';
-import { Books } from './components/Books';
+import { Home } from './components/Home';
 
 class App extends React.Component {
   render() {
-    const books = loadBooks();
     return (
       <div className="container">
         <Header/>
-        <Scroller/>
-        <Books books={books}/>
+        <Home/>
       </div>
     );
   };
 }
 
 render(<App />, window.document.getElementById('app'));
-
-function loadBooks() {
-  return [{
-      "id": "1",
-      "name": "For Whom The Bell Tolls",
-      "image": "https://s3-ap-southeast-1.amazonaws.com/rokomari110/productNew/260X372/rokimg20141031_90990.gif",
-      "price": "718",
-      "isbn": "9780099908609",
-      "condition": "New",
-      "description": "Robert Jordan is a young American with the International Brigades attached to an antifascist guerilla unit in the mountains of Spain. He is an experienced dynamiter, and he is tasked by a communist Russian general to",
-      "category": [
-        "Drama",
-        "War",
-        "History"
-      ],
-      "author": "Ernest Hemingway"
-    },
-    {
-      "id": "2",
-      "name": "To Have And Have Not",
-      "image": "https://s3-ap-southeast-1.amazonaws.com/rokomari110/productNew/260X372/rokimg_20140716_84519.gif",
-      "price": "1895",
-      "isbn": "9780684818986",
-      "condition": "New",
-      "description": "'To Have and Have Not' is the dramatic story of Harry Morgan, an honest man who is forced into running contraband between Cuba and Key West as a means of keeping his crumbling family financially afloat",
-      "category": [
-        "Romance"
-      ],
-      "author": "Ernest Hemingway"
-    }];
-}
