e0167748947cedfa8111ff254c573e1b8176a4bf
emu <himtiaz@dohatec.com.bd>
2018-04-26 15:40:01 +0000

    basic Router setup

--- src/app/components/BookDetail.js
+++ src/app/components/BookDetail.js
@@ -1,39 +1,10 @@
-// It needs to be change
-
 import React from 'react';
 
 export class BookDetail extends React.Component {
-  constructor(props){
-    super();
-    this.state = {
-      rate: props.detail.rating
-    }
-  }
-
-  upvote(){
-    this.setState({
-      rate: this.state.rate + 1
-    });
-  }
-
   render() {
     return (
       <div className="col-md-6">
-        <div className="card flex-md-row mb-4 box-shadow h-md-250">
-          <div className="card-body d-flex flex-column align-items-start">
-            <strong className="d-inline-block mb-2 text-success">Design</strong>
-            <h3 className="mb-0">
-              <a className="text-dark" href="#">{this.props.detail.name}</a>
-            </h3>
-            <div className="mb-1 text-muted">Rating: {this.state.rate}</div>
-            <p className="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to
-              additional content.</p>
-            <a onClick={() => this.upvote()}>Upvote</a>
-          </div>
-          <img className="card-img-right flex-auto d-none d-lg-block book-cover"
-               alt="Thumbnail [200x250]"
-               src={this.props.detail.image}/>
-        </div>
+        Hello: Edit me
       </div>
     );
   }
--- src/app/index.js
+++ src/app/index.js
@@ -1,19 +1,24 @@
 import React from "react";
 import {render} from "react-dom";
+import {BrowserRouter, Route, Link, IndexRoute} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 import styles from './styles/blog.css';
 
-import { Header } from './components/Header';
-import { Home } from './components/Home';
+import {Root} from './components/Root';
+import {Home} from './components/Home';
+import {BookDetail} from './components/BookDetail';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="container">
-        <Header/>
-        <Home/>
-      </div>
+      <BrowserRouter>
+        <div className="container">
+          <Route path='/' component={Root}/>
+          <Route exact path="/" component={Home}/>
+          <Route path='/book' component={BookDetail} />
+        </div>
+      </BrowserRouter>
     );
   };
 }
