f5e5bef1429f8e22b447c4c9c8b824b50f2edeac
imtiaz-emu <imtiaz.emu.it@gmail.com>
2018-05-11 22:01:21 +0600

    Added Heroku API endpoint

--- src/app/utils/EndPoint.js
+++ src/app/utils/EndPoint.js
@@ -1,5 +1,5 @@
 export default {
-  api_url: 'http://localhost:3000/api/v1/',
+  api_url: 'https://cryptic-badlands-35281.herokuapp.com/api/v1/',
   api_token: 'Bearer ' + localStorage.getItem('jwtToken'),
   is_logged_in: function () {
     return localStorage.getItem('jwtToken') != null;
